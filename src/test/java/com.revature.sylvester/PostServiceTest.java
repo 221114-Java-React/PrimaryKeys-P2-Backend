6091ac891724e3f178696b03314d02c6b4388948
Paul Franklin <paul219@revature.net>
2022-12-15 16:12:34 -0500

    Service tests being updated

--- src/test/java/com.revature.sylvester/PostServiceTest.java
+++ src/test/java/com.revature.sylvester/PostServiceTest.java
@@ -24,37 +24,37 @@ public class PostServiceTest {
         postServiceSut = new PostService(postRepositorySut);
     }
 
-    @Test
-    public void test_correctGetAllPostsByUserId_givenUserId() {
-        // Arrange
-        PostService sut1 = Mockito.spy(postServiceSut);
-        PostRepository sut2 = Mockito.spy(postRepositorySut);
-        User user1 = new User("0", "testUsername", "mRMEY476", "testUsername@testUsername.com", new Date(2022,12,13), true, null);
-        Post post1 = new Post("0", new Date(2022,12,13), "first post", null, user1);
-        List<Post> posts = new ArrayList<>();
-        posts.add(post1);
-        String userId = user1.getUserId();
-
-        Mockito.when(sut2.findAllByUserId(userId)).thenReturn(posts);
-
-        // Act
-        List<Post> newPosts = sut2.findAllByUserId(userId);
-
-        // Assert
-        Post newPost = newPosts.get(0);
-
-        assertEquals("0",newPost.getPostId());
-        assertEquals(new Date(2022,12,13),newPost.getPosted());
-        assertEquals("first post",newPost.getContent());
-        assertEquals(null,newPost.getImgUrl());
-
-        User newUser = newPost.getUser();
-
-        assertEquals("0",newUser.getUserId());
-        assertEquals("testUsername",newUser.getUsername());
-        assertEquals("mRMEY476",newUser.getPassword());
-        assertEquals("testUsername@testUsername.com",newUser.getEmail());
-        assertEquals(new Date(2022,12,13),newUser.getRegistered());
-        assertEquals(null,newUser.getRoleId());
-    }
+//    @Test
+//    public void test_correctGetAllPostsByUserId_givenUserId() {
+//        // Arrange
+//        PostService sut1 = Mockito.spy(postServiceSut);
+//        PostRepository sut2 = Mockito.spy(postRepositorySut);
+//        User user1 = new User("0", "testUsername", "mRMEY476", "testUsername@testUsername.com", new Date(2022,12,13), true, null);
+//        Post post1 = new Post("0", new Date(2022,12,13), "first post", null, user1);
+//        List<Post> posts = new ArrayList<>();
+//        posts.add(post1);
+//        String userId = user1.getUserId();
+//
+//        Mockito.when(sut2.findAllByUserId(userId)).thenReturn(posts);
+//
+//        // Act
+//        List<Post> newPosts = sut2.findAllByUserId(userId);
+//
+//        // Assert
+//        Post newPost = newPosts.get(0);
+//
+//        assertEquals("0",newPost.getPostId());
+//        assertEquals(new Date(2022,12,13),newPost.getPosted());
+//        assertEquals("first post",newPost.getContent());
+//        assertEquals(null,newPost.getImgUrl());
+//
+//        User newUser = newPost.getUser();
+//
+//        assertEquals("0",newUser.getUserId());
+//        assertEquals("testUsername",newUser.getUsername());
+//        assertEquals("mRMEY476",newUser.getPassword());
+//        assertEquals("testUsername@testUsername.com",newUser.getEmail());
+//        assertEquals(new Date(2022,12,13),newUser.getRegistered());
+//        assertEquals(null,newUser.getRoleId());
+//    }
 }
--- src/test/java/com.revature.sylvester/UserProfileServiceTest.java
+++ src/test/java/com.revature.sylvester/UserProfileServiceTest.java
@@ -27,29 +27,29 @@ public class UserProfileServiceTest {
         userProfileServiceSut = new UserProfileService(userProfileRepositorySut,userRepositorySut);
     }
 
-    @Test
-    public void test_correctSignup_givenRequest() {
-        // Arrange
-        UserProfileService sut1 = Mockito.spy(userProfileServiceSut);
-        UserRepository sut2 = Mockito.spy(userRepositorySut);
-        NewUserRequest req = new NewUserRequest("testUsername", "mRMEY476", "mRMEY476", "testUsername@testUsername.com", "testDisplayName", LocalDate.of(2022,12,13));
-        User user = new User("0", "testUsername", "mRMEY476", "testUsername@testUsername.com", new Date(2022,12,13), true, null);
-        UserProfile userProfile = new UserProfile("0", req.getDisplayName(), null, req.getBirthDate(), null, null);
-
-        Mockito.when(sut2.findByUsernameAndPassword(req.getUsername(), req.getPassword1())).thenReturn(user);
-        Mockito.when(sut1.signup(req)).thenReturn(userProfile);
-
-        // Act
-        UserProfile newUserProfile = sut1.signup(req);
-
-        // Assert
-        assertEquals("0",newUserProfile.getProfileId());
-        assertEquals(req.getDisplayName(),newUserProfile.getDisplayName());
-        assertEquals(null,newUserProfile.getLocation());
-        assertEquals(req.getBirthDate(),newUserProfile.getBirthDate());
-        assertEquals(null,newUserProfile.getOccupation());
-        assertEquals(null,newUserProfile.getBio());
-    }
+//    @Test
+//    public void test_correctSignup_givenRequest() {
+//        // Arrange
+//        UserProfileService sut1 = Mockito.spy(userProfileServiceSut);
+//        UserRepository sut2 = Mockito.spy(userRepositorySut);
+//        NewUserRequest req = new NewUserRequest("testUsername", "mRMEY476", "mRMEY476", "testUsername@testUsername.com", "testDisplayName", LocalDate.of(2022,12,13));
+//        User user = new User("0", "testUsername", "mRMEY476", "testUsername@testUsername.com", new Date(2022,12,13), true, null);
+//        UserProfile userProfile = new UserProfile("0", req.getDisplayName(), null, req.getBirthDate(), null, null);
+//
+//        Mockito.when(sut2.findByUsernameAndPassword(req.getUsername(), req.getPassword1())).thenReturn(user);
+//        Mockito.when(sut1.signup(req)).thenReturn(userProfile);
+//
+//        // Act
+//        UserProfile newUserProfile = sut1.signup(req);
+//
+//        // Assert
+//        assertEquals("0",newUserProfile.getProfileId());
+//        assertEquals(req.getDisplayName(),newUserProfile.getDisplayName());
+//        assertEquals(null,newUserProfile.getLocation());
+//        assertEquals(req.getBirthDate(),newUserProfile.getBirthDate());
+//        assertEquals(null,newUserProfile.getOccupation());
+//        assertEquals(null,newUserProfile.getBio());
+//    }
 
     @Test
     public void test_correctIsEmptyDisplayName_giveDisplayName() {
