74d8e884736ad390a21825b0914455744860ecff
Paul Franklin <paul219@revature.net>
2022-12-16 15:53:18 -0500

    CommentController create

--- src/main/java/com/revature/sylvester/controllers/CommentController.java
+++ src/main/java/com/revature/sylvester/controllers/CommentController.java
@@ -1,10 +1,15 @@
 package com.revature.sylvester.controllers;
 
+import com.revature.sylvester.dtos.requests.NewCommentRequest;
+import com.revature.sylvester.dtos.responses.Principal;
 import com.revature.sylvester.entities.Like;
 import com.revature.sylvester.services.CommentService;
 import com.revature.sylvester.services.TokenService;
+import com.revature.sylvester.utils.custom_exceptions.InvalidAuthException;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @CrossOrigin
@@ -19,8 +24,32 @@ public class CommentController {
         this.commentService = commentService;
     }
 
+    @PostMapping
+    public void create(@RequestBody NewCommentRequest req, HttpServletRequest servReq) {
+        String token = servReq.getHeader("authorization");
+
+        if(token == null || token.isEmpty())
+            throw new InvalidAuthException("Invalid token");
+
+        Principal principal = tokenService.extractRequesterDetails(token);
+
+        if(principal == null)
+            throw new InvalidAuthException("Please log in to create a comment");
+
+        if(!principal.isActive())
+            throw new InvalidAuthException("Your account is not active");
+
+        commentService.createComment(req, principal.getUserId());
+    }
+
     @GetMapping("/post")
     public List<Like> getAllByPostId(@RequestParam String id) {
         return commentService.getAllCommentsByPostId(id);
+    }
+
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    @ExceptionHandler(InvalidAuthException.class)
+    public InvalidAuthException handledAuthException (InvalidAuthException e) {
+        return e;
     }
 }
--- src/main/java/com/revature/sylvester/repositories/CommentRepository.java
+++ src/main/java/com/revature/sylvester/repositories/CommentRepository.java
@@ -2,12 +2,19 @@ package com.revature.sylvester.repositories;
 
 import com.revature.sylvester.entities.Comment;
 import com.revature.sylvester.entities.Like;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Date;
 import java.util.List;
 
 public interface CommentRepository extends CrudRepository<Comment, String> {
+    @Modifying
+    @Query(value = "INSERT INTO replies(reply_id, reply, replied, user_id, post_id) VALUES (?1, ?2, ?3, ?4, ?5)",
+            nativeQuery = true)
+    void save(String replyId, String reply, Date replied, String userId, String postId);
+
     @Query(value = "SELECT * FROM replies WHERE post_id = ?1", nativeQuery = true)
     List<Like> findAllByPostId(String postId);
 }
