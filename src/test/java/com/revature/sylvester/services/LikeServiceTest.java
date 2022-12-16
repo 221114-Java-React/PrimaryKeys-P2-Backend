0c0109924ccb8d5f10bf61b085ee54db333a71d4
ScruffyC <118226061+ScruffyC@users.noreply.github.com>
2022-12-16 16:03:45 -0600

    Update LikeServiceTest.java

--- src/test/java/com/revature/sylvester/services/LikeServiceTest.java
+++ src/test/java/com/revature/sylvester/services/LikeServiceTest.java
@@ -1,9 +1,14 @@
 package com.revature.sylvester.services;
 
+import com.revature.sylvester.entities.Like;
 import com.revature.sylvester.repositories.LikeRepository;
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -19,10 +24,16 @@ class LikeServiceTest {
     @Test
     void test_saveLikeByUserIdAndPostId_givenValidUserIdAndPostId() {
         // Arrange
+        LikeService spySut = Mockito.spy(sut);
+        String validUserId = UUID.randomUUID().toString();
+        String validPostId = UUID.randomUUID().toString();
 
         // Act
+        spySut.saveLikeByUserIdAndPostId(validUserId, validPostId);
 
         // Assert
+        Mockito.verify(mockLikeRepo, Mockito.times(1)).save(UUID.randomUUID().toString(),
+                validUserId, validPostId);
     }
 
     @Test
@@ -30,7 +41,17 @@ class LikeServiceTest {
     }
 
     @Test
-    void test_getAllLikesByUserId() {
+    void test_getAllLikesByUserId_givenValidUserId() {
+        // Arrange
+        String validUserId = UUID.randomUUID().toString();
+        List<Like> stubbedLikes = Arrays.asList(new Like(), new Like(), new Like());
+        Mockito.when(mockLikeRepo.findAllByUserId(validUserId)).thenReturn(stubbedLikes);
+
+        // Act
+        List<Like> condition = sut.getAllLikesByUserId(validUserId);
+
+        // Assert
+        assertFalse(condition.isEmpty());
     }
 
     @Test
